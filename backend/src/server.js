6f0effcc663c7655a848ea78ec3c3e1c656770f7
Sumit-Tiwari830 <kiransumit2231@gmail.com>
2026-05-22 06:41:58 +0000

    auth and db sync
    Signed-off-by: Sumit-Tiwari830 <kiransumit2231@gmail.com>

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,21 @@
 import express from 'express';
 import path from 'path';
-import dotenv from 'dotenv';
+import cors from 'cors';
+import { serve } from "inngest/express";
+
+
 import { ENV } from './lib/env.js';
 import { connectDB } from "./lib/db.js";
+import { inngest, functions } from "./lib/inngest.js";
 const app = express();
 
 //console.log('PORT:', ENV.PORT);
 //console.log('DB_URL:', ENV.DB_URL);
 const __dirname = path.resolve();
+app.use(express.json());
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+
+app.use("/api/inngest", serve({ client: inngest, functions }))
 
 app.get('/health', (req, res) => {
     res.status(200).json({ message: 'Hello World health!123' });
@@ -24,9 +32,9 @@ if (ENV.NODE_ENV === "production") {
 }
 const startServer = async () => {
     try {
-          if (!ENV.DB_URL) {
-        throw new Error("DB_URL is not defined in environment variables");
-   }
+        if (!ENV.DB_URL) {
+            throw new Error("DB_URL is not defined in environment variables");
+        }
         await connectDB();
         app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
     } catch (error) {

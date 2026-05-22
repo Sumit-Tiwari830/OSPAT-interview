2a33c4cd46def922b90b684c4861c18fb82443df
Sumit-Tiwari830 <kiransumit2231@gmail.com>
2026-05-22 03:47:14 +0000

    validation check added for DB_URL
    Signed-off-by: Sumit-Tiwari830 <kiransumit2231@gmail.com>

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,10 +24,14 @@ if (ENV.NODE_ENV === "production") {
 }
 const startServer = async () => {
     try {
+          if (!ENV.DB_URL) {
+        throw new Error("DB_URL is not defined in environment variables");
+   }
         await connectDB();
         app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
     } catch (error) {
         console.error("💥 Error starting the server", error);
+        process.exit(1);
     }
 };
 startServer();

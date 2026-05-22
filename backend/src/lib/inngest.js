7b4241fa2b42959158f2eebcadc0b65ea7603f25
Sumit-Tiwari830 <kiransumit2231@gmail.com>
2026-05-22 10:54:50 +0000

    auth & stream sync
    Signed-off-by: Sumit-Tiwari830 <kiransumit2231@gmail.com>

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,7 +1,7 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import User from "../models/User.js";
-//import { deleteStreamUser, upsertStreamUser } from "./stream.js";
+import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 
 export const inngest = new Inngest({ id: "ospat" });
 
@@ -21,6 +21,11 @@ const syncUser = inngest.createFunction(
         };
 
         await User.create(newUser);
+        await upsertStreamUser({
+            id: newUser.clerkId.toString(),
+            name: newUser.name,
+            image: newUser.profileImage,
+        });
     }
 );
 
@@ -32,7 +37,7 @@ const deleteUserFromDB = inngest.createFunction(
 
         const { id } = event.data;
         await User.deleteOne({ clerkId: id });
-
+        await deleteStreamUser(id.toString());
 
     }
 );

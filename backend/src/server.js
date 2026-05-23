0b3ee5526ac14a3a35880d88b7e8ca14bcf95623
Sumit-Tiwari830 <kiransumit2231@gmail.com>
2026-05-23 03:21:11 +0000

    server side auth implemented
    Signed-off-by: Sumit-Tiwari830 <kiransumit2231@gmail.com>

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,13 @@ import express from 'express';
 import path from 'path';
 import cors from 'cors';
 import { serve } from "inngest/express";
-
+import { clerkMiddleware } from '@clerk/express'
 
 import { ENV } from './lib/env.js';
 import { connectDB } from "./lib/db.js";
 import { inngest, functions } from "./lib/inngest.js";
+//import { protectRoute } from "./middleware/protectRoute.js";
+import chatRoutes from "./routes/chatRoutes.js";
 const app = express();
 
 //console.log('PORT:', ENV.PORT);
@@ -14,15 +16,20 @@ const app = express();
 const __dirname = path.resolve();
 app.use(express.json());
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware());
 
 app.use("/api/inngest", serve({ client: inngest, functions }))
-
+app.use("/api/chat", chatRoutes)
 app.get('/health', (req, res) => {
     res.status(200).json({ message: 'Hello World health!123' });
 });
 app.get('/books', (req, res) => {
     res.status(200).json({ message: 'Hello World!123 books' });
 });
+app.get('/video-calls', (req, res) => {
+    res.status(200).json({ message: 'Hello World!123 video calls' });
+});
+
 if (ENV.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 

cbcc708cb545aa05dff1f9e17f304fa319af4047
Sumit-Tiwari830 <kiransumit2231@gmail.com>
2026-05-24 03:25:39 +0000

    frontend tool setup
    Signed-off-by: Sumit-Tiwari830 <kiransumit2231@gmail.com>

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,8 @@ import { ClerkProvider } from "@clerk/clerk-react";
 
 import "./index.css";
 import App from "./App.jsx";
+import { BrowserRouter } from "react-router"
+import { QueryClient, QueryClientProvider, useQuery } from "@tanstack/react-query";
 
 const PUBLISHABLE_KEY =
   import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -14,8 +16,12 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
-    </ClerkProvider>
+    <BrowserRouter>
+      <QueryClientProvider client={QueryClient}>
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+          <App />
+        </ClerkProvider>
+      </QueryClientProvider>k
+    </BrowserRouter>
   </StrictMode>
 );
